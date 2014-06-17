ced7b2dd48d1c3e1ea59f2c4d0909ab5ef45d53d
Tom Anderson <tom.anderson@timgroup.com>
2014-06-17 19:45:30 +0100

    With some sugar for the higher-kinded type

--- src/main/scala/Idea.scala
+++ src/main/scala/Idea.scala
@@ -19,42 +19,77 @@ duplication.
 Two weaknesses with this approach are that it requires redundant Option
 boilerplate (eg having to pass Some(x) instead of x to a constructor) and that
 it involves a scary wizard-level Scala feature, the higher-kinded type.
+
+Some simple type definitions and methods in a companion object provide
+convenient names and factories for objects in each of the lifecycle stages
+which make them usable in much the same way as fully-fledged case classes.
+Further type definitions give names to sets of lifecycle stages which have some
+coherent meaning. Finally, a couple of implicit conversions ease the
+interconversion of values and Somes. Outside the factory methods, this is
+principally useful in code which reads the fields declared with higher-kinded
+types, as it gets rid of boilerplate Option::get calls.
+
+None of the syntactic sugar in the companion object is necessary, and any bit
+of it can be used in isolation from any other bit.
 */
 
 case class Idea[+IfSaved[_] <: Option[_], +IfClosed[_] <: Option[_]](id: IfSaved[String], stock: String, openDate: Date, closeDate: IfClosed[Date])
 
+object Idea {
+	type None_[T] = Option[Nothing] // long story
+	
+	type UnsavedIdea = Idea[None_,  None_]
+	type SavedIdea = Idea[Some, None_]
+	type ClosedIdea = Idea[Some, Some]
+	
+	implicit def thing2Some[T](t: T): Some[T] = Some(t)
+	implicit def some2Thing[T](s: Some[T]): T = s.get
+	
+	def UnsavedIdea(stock: String, openDate: Date): UnsavedIdea = Idea(None: None_[String], stock, openDate, None: None_[Date])
+	def SavedIdea(id: String, stock: String, openDate: Date): SavedIdea = Idea(id, stock, openDate, None: None_[Date])
+	def ClosedIdea(id: String, stock: String, openDate: Date, closeDate: Date): ClosedIdea = Idea(id, stock, openDate, closeDate)
+	
+	type AnyIdea = Idea[Option, Option]
+	type AnySavedIdea = Idea[Some, Option]
+	type AnyClosedIdea = Idea[Option, Some]
+}
+
 object FunctionsThatTakeIdeas {
+	import Idea._
+	
 	// something that does not require any additional fields
-	def format(idea: Idea[Option, Option]): String = {
+	def format(idea: AnyIdea): String = {
 		idea.stock + "@" + idea.openDate
 	}
 	
 	// something that requires an ID
-	def query(idea: Idea[Some, Option]): String = {
-		idea.id.get
+	def query(idea: AnySavedIdea): String = {
+		idea.id
 	}
 	
 	// something that requires a close date
-	def duration(idea: Idea[Option, Some]): Long = {
-		idea.closeDate.get.getTime - idea.openDate.getTime
+	def duration(idea: AnyClosedIdea): Long = {
+		idea.closeDate.getTime - idea.openDate.getTime
 	}
 }
 
 object Main {
 	def main(args: Array[String]): Unit = {
-		val unsaved = Idea(None, "VOD.L", new Date(), None)
+		import Idea._
+		
+		val unsaved: UnsavedIdea = UnsavedIdea("VOD.L", new Date())
 		println("unsaved: " + unsaved)
 		FunctionsThatTakeIdeas.format(unsaved)
 		// FunctionsThatTakeIdeas.query(unsaved) // forbidden
 		// FunctionsThatTakeIdeas.duration(unsaved) // forbidden
 		
-		val saved = unsaved.copy(id = Some("beefc4c3"))
+		val saved: SavedIdea = SavedIdea("beefc4c3", "VOD.L", new Date())
 		println("saved: " + saved)
 		FunctionsThatTakeIdeas.format(saved)
 		FunctionsThatTakeIdeas.query(saved)
 		// FunctionsThatTakeIdeas.duration(saved) // forbidden
 		
-		val closed = saved.copy(closeDate = Some(new Date()))
+		val closed: ClosedIdea = ClosedIdea("beefc4c3", "VOD.L", new Date(), new Date())
 		println("closed: " + closed)
 		FunctionsThatTakeIdeas.format(closed)
 		FunctionsThatTakeIdeas.query(closed)
